632a9c41e44be5d8f5281e43e553a637c173ac6e
Tonatew <42447015+Tonatew@users.noreply.github.com>
2021-06-27 01:39:50 -0500

    Fixed bugs and implemented download image

--- Scripts CCSS/PatternGenerator.js
+++ Scripts CCSS/PatternGenerator.js
@@ -1,4 +1,4 @@
-var selectedTexel = "none"
+var selectedTexel = "none";
 
 const texelSizes = {
   small: 128,
@@ -12,31 +12,33 @@ function changePrintSize() {
   document.getElementById("printWidth").value = pageSizes[selectedSize].width;
   document.getElementById("printHeight").value = pageSizes[selectedSize].height;
 
-  configurePrintArea()
+  configurePrintArea();
 }
 
-function changeTexel(imgElement){
-    selectedTexel = imgElement.src
-    printArea.style.backgroundImage = `url("${selectedTexel}")`
-    configurePrintArea()
+function changeTexel(imgElement) {
+  selectedTexel = imgElement.src;
+  printArea.style.backgroundImage = `url("${selectedTexel}")`;
+  configurePrintArea();
+  location.href = "#";
+  location.href = "#page";
 }
- 
+
 function changeTexelSize() {
-    console.log("Chaging Texel Size");
-    let printArea = document.getElementById('printArea')
-    let selectedSize = document.getElementById('texelSize').value;
-    printArea.style.backgroundSize = `${texelSizes[selectedSize]}px`
-    console.log(printArea.style.backgroundSize)
+  console.log("Chaging Texel Size");
+  let printArea = document.getElementById("printArea");
+  let selectedSize = document.getElementById("texelSize").value;
+  printArea.style.backgroundSize = `${texelSizes[selectedSize]}px`;
+  console.log(printArea.style.backgroundSize);
 }
 
 function configurePrintArea() {
   var printArea = document.getElementById("printArea");
   let width = document.getElementById("printWidth").value;
-  let height = document.getElementById("printHeight").value
+  let height = document.getElementById("printHeight").value;
 
   //Set page size
-  printArea.style.width = `${width}cm`
-  printArea.style.height = `${height}cm`
+  printArea.style.width = `${width}cm`;
+  printArea.style.height = `${height}cm`;
 }
 
 function loadPatterns() {
@@ -44,36 +46,66 @@ function loadPatterns() {
 
   for (const [key, path] of Object.entries(patterns)) {
     galleryItem = document.createElement("img");
-    galleryItem.onclick = function(){changeTexel(this)}
+    galleryItem.onclick = function () {
+      changeTexel(this);
+    };
     galleryItem.src = path;
     galleryItem.setAttribute("class", "galleryItem");
-    
+
     gallery.appendChild(galleryItem);
   }
 }
 
 function saveAsPDF() {
-    //page orientation and elements count calculation
-    var element = document.getElementById("printArea");
-    var selectedSize = document.getElementById("sizeSelect").value;
-    console.log(pageSizes[selectedSize].jsPDF)
-  
-    var opt = {
-      margin: 0,
-      filename: "Plantilla.pdf",
-      image: { type: "png", quality: 1 },
-      html2canvas: { scale: 2 },
-      jsPDF: { unit: "cm", format: pageSizes[selectedSize].jsPDF, orientation: "portrait" },
-    };
-  
-    // New Promise-based usage:
-    html2pdf().set(opt).from(element).save();
-  }
+  //page orientation and elements count calculation
+  var element = document.getElementById("printArea");
+  var selectedSize = document.getElementById("sizeSelect").value;
+  console.log(pageSizes[selectedSize].jsPDF);
+
+  var opt = {
+    margin: 0,
+    filename: "Plantilla.pdf",
+    image: { type: "png", quality: 1 },
+    html2canvas: { scale: 2 },
+    jsPDF: {
+      unit: "cm",
+      format: pageSizes[selectedSize].jsPDF,
+      orientation: "portrait",
+    },
+  };
+
+  // New Promise-based usage:
+  html2pdf().set(opt).from(element).save();
+}
+
+function toDataURL() {
+  let url = getComputedStyle(printArea).backgroundImage;
+  url = url.slice(5, url.length - 2);
+  return fetch(url)
+    .then((response) => {
+      return response.blob();
+    })
+    .then((blob) => {
+      return URL.createObjectURL(blob);
+    });
+}
+
+async function downloadTexel() {
+  const a = document.createElement("a");
+  a.href = await toDataURL();
+  a.download = "patron";
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+}
+
+function goToConfig() {
+  location.href = "#";
+  location.href = "#configPattern";
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   changePrintSize();
   loadPatterns();
-  //configurePrintArea();
-  var printArea = document.getElementById("printArea");
-  console.log(getComputedStyle(printArea).backgroundImage)
+  configurePrintArea();
 });
